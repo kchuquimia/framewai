1701de62569464fce8a3001974b5e8790890ca95
kchuquimia <karina@immigration-consulting.com>
2026-05-28 15:34:06 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('FramewAI - Workforce Mobility Operations');
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('FramewAI server running');
+});
